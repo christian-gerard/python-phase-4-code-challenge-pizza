56a1af6f540332a48ac6e90ab3a9be864a661d1b
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-04-06 17:43:06 -0600

    Everything Done, last test is not passing for some reason

--- server/app.py
+++ server/app.py
@@ -19,10 +19,87 @@ db.init_app(app)
 
 api = Api(app)
 
+class Restaurants(Resource):
+    def get(self):
+        try:
+            data = [restaurant.to_dict(rules=('-restaurant_pizzas',)) for restaurant in Restaurant.query]
+            return data, 200
+        
+        except Exception as e:
+            return str(e), 404
 
-@app.route("/")
-def index():
-    return "<h1>Code challenge</h1>"
+class RestaurantById(Resource):
+    def get(self,id):
+            try:
+                data = Restaurant.query.filter(Restaurant.id == id).first()
+
+                if isinstance(data, Restaurant):
+                    return data.to_dict(rules=('restaurant_pizzas',)), 200
+                else:
+                    return {"error" : "Restaurant not found"}, 404
+                
+            except Exception as e:
+                return str(e), 404
+            
+    def delete(self,id):
+        try:
+            data = Restaurant.query.filter(Restaurant.id == id).first()
+
+            if isinstance(data, Restaurant):
+                db.session.delete(data)
+                db.session.commit()
+                return '', 204
+            else:
+                return {'error': 'restaurant not found'}, 404
+
+        except Exception as e:
+            return str(e), 404
+            
+class Pizzas(Resource):
+    def get(self):
+        try:
+            data = [Pizza.to_dict(rules=('-restaurant_pizzas',)) for Pizza in Pizza.query]
+            return data, 200
+        
+        except Exception as e:
+            return str(e), 404
+
+class PizzaById(Resource):
+    def get(self,id):
+        try:
+            data = Pizza.query.filter(Pizza.id == id).first()
+
+            if isinstance(data, Pizza):
+                return data.to_dict(only=('restaurant_pizzas',)), 200
+            else:
+                return {"error" : "Restaurant not found"}, 404
+            
+        except Exception as e:
+            return str(e), 404
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            new_rp = RestaurantPizza(**data)
+            if isinstance(new_rp, RestaurantPizza):
+                db.session.add(new_rp)
+                db.session.commit()
+                return new_rp.to_dict(only=('id','price','restaurant_id','pizza_id','pizza','restaurant')), 201
+            else:
+                return {'errors': ['validation errors']}, 400
+
+        except Exception as e:
+            return {'errors': ['validation errors']}, 400
+
+
+
+
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
+api.add_resource(RestaurantById, '/restaurants/<int:id>')
+api.add_resource(PizzaById, '/pizzas/<int:id>')
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -21,8 +21,10 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza',cascade='all, delete-orphan', back_populates='restaurant')
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +38,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza',cascade='all, delete-orphan', back_populates='pizza')
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -48,12 +52,27 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
     # add relationships
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
 
     # add serialization rules
+    serialize_rules = ('-restaurant', '-pizza')
 
     # add validation
+    @validates('price')
+    def validate_price(self, key, price):
+        if not isinstance(price, int):
+            raise ValueError('Must be int')
+        elif price > 30 or price < 1:
+            raise ValueError('error')
+        else:
+            return price
+
+
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
